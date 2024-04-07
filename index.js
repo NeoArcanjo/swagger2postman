60e24ce797b171bf0ff8225723eef927716d2c7e
Rafael Arcanjo <rexmundi.rafael@gmail.com>
2024-04-07 15:43:40 -0300

    Add dependencies and update fetch.js

--- index.js
+++ index.js
@@ -6,81 +6,97 @@ process.env.SUPPRESS_NO_CONFIG_WARNING = 'y';
 var configModule = require('config')
 config = configModule.util.loadFileConfigs(__dirname + '/config/')
 const fetch = require('./lib/fetch')
-const merger=require('./lib/merger')
-
+const merger = require('./lib/merger')
+const cliSpinners = require('cli-spinners');
+const Spinnies = require('spinnies');
 const program = require('commander')
 program.version('1.0.0')
-    .option('-s --service <service>', 'which service to convert')
-    .option('-r --replace [repliaces]', 'comma split api name which will replace not merge')
-    .parse(process.argv)
+  .option('-s --service <service>', 'which service to convert')
+  .option('-r --replace [repliaces]', 'comma split api name which will replace not merge')
+  .parse(process.argv)
 
 
 var serviceConfig = config[program.service]
 var url = serviceConfig.url
 var collectionName = serviceConfig.collection_name
 
+const spinner = new Spinnies({
+  color: 'blueBright',
+  spinnerColor: 'blueBright',
+  succeedColor: 'green',
+  spinner: cliSpinners.dots,
+});
+
 //run update
 update().catch(err => {
-    console.error("run failed," + err)
+  console.error("run failed," + err)
 })
 
 //get swagger json
 function getSwaggerJson(url) {
-    return fetch({
-        url: url,
-        methods: 'get'
-    }).then(response => {
-        return response.data
-    }).catch(err => {
-        console.log('get swagger json failed: ' + err.message)
-        process.exit(-1);
-    })
+  return fetch({
+    url: url,
+    methods: 'get'
+  }).then(response => {
+    return response.data
+  }).catch(err => {
+    console.log(err);
+    console.log('get swagger json failed: ' + err.message);
+    process.exit(-1);
+  })
 }
-
-
 
 async function update() {
-    var swaggerJson = await getSwaggerJson(url)
-    //add postman collection used info
-    swaggerJson['info'] = {
-        'title': collectionName,
-        'description': collectionName + ' api',
-        'version': '1.0.0',
-        '_postman_id': '807bb824-b333-4b59-a6ef-a8d46d3b95bf'
+  spinner.add('update', { text: 'Starting update' });
+  var swaggerJson = await getSwaggerJson(url)
+  //add postman collection used info
+  swaggerJson['info'] = {
+    'title': collectionName,
+    'description': collectionName + ' api',
+    'version': '1.0.0',
+    '_postman_id': '807bb824-b333-4b59-a6ef-a8d46d3b95bf'
+  }
+  var converterInputData = {
+    'type': 'json',
+    'data': swaggerJson
+  }
+
+  spinner.update('update', { text: 'Converting to postman collection' });
+
+  //use postman tool convert to postman collection
+  converter.convert(converterInputData, { 'folderStrategy': 'Tags' }, async (_a, res) => {
+    if (res.result === false) {
+      console.log('convert failed')
+      console.log(res.reason)
+      return
     }
-    var converterInputData = {
-        'type': 'json',
-        'data': swaggerJson
+    var convertedJson = res.output[0].data
+
+    var id = await collection.getCollectionId(collectionName)
+    if (id === null) {
+      return
+    }
+    var collectionJson = {
+      'collection': {
+        'info': {
+          'name': collectionName,
+          'description': collectionName + ' api',
+          '_postman_id': id,
+          "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+
+        },
+        "item": convertedJson.item
+      }
     }
 
-    //use postman tool convert to postman collection
-    converter.convert(converterInputData, { 'folderStrategy': 'Tags' }, async (_a, res) => {
-        if (res.result === false) {
-            console.log('convert failed')
-            console.log(res.reason)
-            return
-        }
-        var convertedJson = res.output[0].data
+    var savedCollection = await collection.getCollectionDetail(id)
+    var mergedCollection = merger.merge(savedCollection, collectionJson)
 
-        var id = await collection.getCollectionId(collectionName)
-        if (id === null) {
-            return
-        }
-        var collectionJson = {
-            'collection': {
-                'info': {
-                    'name': collectionName,
-                    'description': collectionName + ' api',
-                    '_postman_id': id,
-                    "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
-
-                },
-                "item": convertedJson.item
-            }
-        }
-    
-        var savedCollection = await collection.getCollectionDetail(id)   
-        var mergedCollection=merger.merge(savedCollection,collectionJson)    
-        collection.updateCollection(id, mergedCollection)
-    })
+    spinner.update('update', { text: 'Updating collection' });
+    collection.updateCollection(id, mergedCollection, spinner)
+  })
 }
+
+module.exports = {
+  spinner,
+};
